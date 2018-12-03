cdcf5ef41cbf08f6a1d16077de5b0bf62e9b7916
pjdoro <45492769+pjdoro@users.noreply.github.com>
2018-12-02 19:21:46 -0700

    Update singleUpdate.py

--- singleUpdate.py
+++ singleUpdate.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import requests
 # indicate the row number in the csv to post update
-rec = 0
+user = 0
 # Specify target API and valid key
 api_key = '123' #This is a placeholder
 # Read csv into dataframe
 data = 'Tomato_data.csv'
 df = pd.read_csv(data)
 # Create payload for POST request
-payload =[(x,df.iloc[rec][x]) for x in list(df.columns.values)]
+payload =[(x,df.iloc[user][x]) for x in list(df.columns.values)]
 #Submit POST request to API
 r = requests.post("https://api.iterable.com/api/users/update", data=payload) #pass valid API_key into this function
 
